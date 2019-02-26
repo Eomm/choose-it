b24d6d15b8964d7012f9987827041fbc92937a5e
Manuel Spigolon <behemoth89@gmail.com>
2019-02-26 20:36:55 +0100

    add: basic implementation with BFS

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # use-it
+
+This module let you focus on the business logic instead of going crazy in a `if/else` jungle 🐵🌴
+
+An example use case is when you have a set of databases connections that are not configured as cluster,
+or simply some settings you need to use based on some logic of your company.
+
+In this case you have to write always the same `if` conditions to pick one of those resources.
+
+`use-it` will solve this problem!
+
+### Usage
+
+```js
+const exampleConfig = {
+  admin: {
+    viewAll: true
+  },
+  external: {
+    viewAll: false,
+    login: 'http://external.login.log'
+  },
+  guest: {
+    viewAll: false,
+    login: 'http://login.log'
+  }
+}
+
+
+const UseIt = require('use-it')
+const resouceChooser = new UseIt()
+
+resouceChooser.addCriteria((item) => item.admin === true, exampleConfig.admin)
+
+// You can chain the Criteria to build sub-conditions
+resouceChooser.addCriteria((item) => item.guest === true, exampleConfig.guest)
+              .addCriteria((item) => item.external === true, exampleConfig.external)
+
+const user = {
+  guest: true,
+  external: true
+}
+
+const res = resouceChooser.breadthFirstSearch(user)
+console.log(res)
+/** It will print out
+   [ 
+      { viewAll: false, login: 'http://login.log' },
+      { viewAll: false, login: 'http://external.login.log' } 
+   ]
+*/
+```
--- index.js
+++ index.js
@@ -1,22 +1,70 @@
 'use strict'
 
-const { BSTree } = require('buckets-js')
+const archy = require('archy')
 
-function compare () {
+function noop () { return true }
+
+function UseIt (fn, resource) {
+  this.parent = undefined
+  this.children = []
+  this.resource = resource
+  this.criteria = fn || noop
+}
+
+UseIt.prototype.addCriteria = function (fn, resource) {
+  const node = new UseIt(fn, resource)
+  node.parent = this
+  this.children.push(node)
+  return node
+}
+
+UseIt.prototype.addSiblingCriteria = function (fn, resource) {
+  return this.addCriteria.call(this.parent, fn, resource)
+}
+
+UseIt.prototype.depthFirstSearch = function (item) {
   // TODO
 }
 
-class useIt {
-  constructor () {
-    this.tree = new BSTree(compare)
+UseIt.prototype.breadthFirstSearch = function (item, opts) {
+  // TODO manage:
+  /*
+    opts.traverseAll: not ignore sub-tree when criteria is false
+  */
+  const resourceStack = []
+  const queue = []
+  queue.push(this)
+
+  while (queue.length > 0) {
+    const node = queue.shift() // ! this is not performant
+    const res = node.criteria(item)
+    if (res === true) { // TODO manage a promise
+      if (node.resource) {
+        resourceStack.push(node.resource)
+      }
+      node.children.forEach(_ => queue.push(_))
+    }
   }
 
-  addResouce (resource, criteria) {
-    this.tree.add(criteria)
-  }
-  addCriteria (criteria) {
-    this.tree.add(criteria)
-  }
+  return resourceStack
 }
 
-module.exports = useIt
+UseIt.prototype.evaluate = function (item, opts) {
+  // TODO traverse the tree
+  return this
+}
+
+UseIt.prototype.prettyPrint = function (pritty) {
+  const getLabel = pritty || ((criteria, resource = '') => `${criteria.toString()} ${resource}`)
+
+  const decorateText = (node) => {
+    node.label = getLabel(node.criteria, node.resource)
+    if (node.children.length > 0) {
+      node.nodes = node.children.map(_ => decorateText(_))
+    }
+    return node
+  }
+  console.log(archy(decorateText(Object.assign({}, this))))
+}
+
+module.exports = UseIt
